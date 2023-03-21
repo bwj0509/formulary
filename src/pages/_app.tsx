c5c7a08016ab7d48b4a494d8053a03a8a590103e
WOOJINBAEK <bwj59@naver.com>
2023-03-21 17:55:23 +0900

    [FEAT] 메타데이터 주입

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,20 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "@/styles/globalStyle";
 import { theme } from "@/styles/theme";
 import Header from "@/components/common/header/header";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Formulary</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="F1에 대한 모든것을 제공합니다"
+          content="74년의 기록, 1120번의 레이스, 858명의 드라이버"
+        ></meta>
+        <meta property="og:image" content="/images/f1history.jpg"></meta>
+      </Head>
       <GlobalStyle />
       <Header />
       <Component {...pageProps} />
